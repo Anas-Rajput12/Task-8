aeb5dfcbe3819e3d10d34dd923df9e344bee6034
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2025-07-23 13:25:13 +0500

    Add files via upload

---
+++ Task 13/script.js
@@ -0,0 +1,15 @@
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+  
+  question.addEventListener('click', () => {
+    const isActive = item.classList.contains('active');
+
+    faqItems.forEach(el => el.classList.remove('active'));
+
+    if (!isActive) {
+      item.classList.add('active');
+    }
+  });
+});
